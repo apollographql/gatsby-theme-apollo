16e8ef0d3a8f8c3b417d7541c4e478a4e7f3f81e
Trevor Blades <tdblades@gmail.com>
2018-11-26 13:35:00 -0800

    Add react-helmet

--- gatsby-theme-apollo/src/components/layout.js
+++ gatsby-theme-apollo/src/components/layout.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
+const title = 'Apollo Documentation';
 export default function Layout(props) {
   return (
     <div>
+      <Helmet defaultTitle={title} titleTemplate={`%s · ${title}`} />
       <header>Apollo</header>
       {props.children}
     </div>
--- gatsby-theme-apollo/src/templates/docs.js
+++ gatsby-theme-apollo/src/templates/docs.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Layout from '../components/layout';
+import Helmet from 'react-helmet';
 
 export default props => {
   return (
     <Layout>
+      <Helmet>
+        <title>{props.pageContext.frontmatter.title}</title>
+      </Helmet>
       {props.children}
     </Layout>
   )
