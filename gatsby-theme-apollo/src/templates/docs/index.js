9ab876fb51390025fa82c638b4a223f587183d1a
Trevor Blades <tdblades@gmail.com>
2019-01-10 12:58:16 -0800

    Add a placeholder main nav

--- gatsby-theme-apollo/src/templates/docs/index.js
+++ gatsby-theme-apollo/src/templates/docs/index.js
@@ -6,7 +6,14 @@ import Search from './search';
 import SidebarNav from './sidebar-nav';
 import VersionSelect from './version-select';
 import styled from '@emotion/styled';
+import {Link} from 'gatsby';
 import {ReactComponent as Logo} from '../../assets/logo.svg';
+
+const colors = {
+  primary: '#220a82',
+  secondary: '#e535ab',
+  divider: '#d8d9e0'
+};
 
 const Container = styled.div({
   display: 'flex',
@@ -19,28 +26,27 @@ const Header = styled.header({
   flexShrink: 0,
   height: 64,
   padding: '0 24px',
-  color: '#220a82',
+  color: colors.primary,
   backgroundColor: 'white',
   position: 'sticky',
   top: 0
 });
 
-const divider = '#d8d9e0';
+const Sidebar = styled.aside({
+  flexShrink: 0,
+  width: 305,
+  borderRight: `1px solid ${colors.divider}`,
+  overflowY: 'auto'
+});
+
 const SidebarHeader = styled(Header)({
-  borderBottom: `1px solid ${divider}`
+  borderBottom: `1px solid ${colors.divider}`
 });
 
 const StyledLogo = styled(Logo)({
   marginRight: 'auto',
   height: 36,
   fill: 'currentColor'
-});
-
-const Sidebar = styled.aside({
-  flexShrink: 0,
-  width: 305,
-  borderRight: `1px solid ${divider}`,
-  overflowY: 'auto'
 });
 
 const SidebarContent = styled.div({
@@ -50,6 +56,27 @@ const SidebarContent = styled.div({
 const Main = styled.main({
   flexGrow: 1,
   overflowY: 'auto'
+});
+
+const Nav = styled.nav({
+  display: 'flex',
+  alignSelf: 'stretch',
+  marginLeft: 'auto'
+});
+
+const NavItem = styled(Link)({
+  display: 'flex',
+  alignItems: 'center',
+  padding: '0 4px',
+  borderBottom: '2px solid transparent',
+  color: colors.primary,
+  textDecoration: 'none',
+  '&.active': {
+    borderColor: colors.secondary
+  },
+  ':not(:last-child)': {
+    marginRight: 24
+  }
 });
 
 const MainContent = styled.div({
@@ -83,6 +110,10 @@ export default function Docs(props) {
         <Main>
           <Header>
             <Search />
+            <Nav>
+              <NavItem>Platform</NavItem>
+              <NavItem className="active">Tutorial</NavItem>
+            </Nav>
           </Header>
           <MainContent>
             <div dangerouslySetInnerHTML={{__html: html}} />
