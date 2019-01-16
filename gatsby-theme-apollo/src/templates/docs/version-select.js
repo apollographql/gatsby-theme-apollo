829c2b0804ce71057f661aca48959b59635b10bc
Trevor Blades <tdblades@gmail.com>
2019-01-16 12:12:24 -0800

    Style the version select

--- gatsby-theme-apollo/src/templates/docs/version-select.js
+++ gatsby-theme-apollo/src/templates/docs/version-select.js
@@ -1,10 +1,39 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
+import colors from '../../util/colors';
 import styled from '@emotion/styled';
 import {navigate} from 'gatsby';
+import {transparentize} from 'polished';
+
+const iconSize = 24;
+const horizontalPadding = 8;
+const Container = styled.div({
+  color: colors.textSecondary,
+  borderRadius: 4,
+  backgroundColor: transparentize(0.75, colors.divider),
+  position: 'relative',
+  '&::after': {
+    content: "'›'",
+    width: iconSize,
+    fontSize: iconSize,
+    lineHeight: `${iconSize}px`,
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    right: 0,
+    transform: 'translateY(-50%) rotate(90deg)',
+    pointerEvents: 'none'
+  }
+});
 
 const Select = styled.select({
-  fontSize: 12
+  appearance: 'none',
+  padding: `6px ${horizontalPadding}px`,
+  paddingRight: horizontalPadding / 2 + iconSize,
+  border: 0,
+  fontSize: 12,
+  color: 'inherit',
+  outline: 'none'
 });
 
 export default class VersionSelect extends Component {
@@ -17,13 +46,15 @@ export default class VersionSelect extends Component {
 
   render() {
     return (
-      <Select value={this.props.value} onChange={this.onVersionChange}>
-        {this.props.versions.map(version => (
-          <option key={version.id} value={version.basePath}>
-            Version {version.majorMinor}
-          </option>
-        ))}
-      </Select>
+      <Container>
+        <Select value={this.props.value} onChange={this.onVersionChange}>
+          {this.props.versions.map(version => (
+            <option key={version.id} value={version.basePath}>
+              Version {version.majorMinor}
+            </option>
+          ))}
+        </Select>
+      </Container>
     );
   }
 }
