08db06a3d43452d0bc08e8a86e465d831148633b
Trevor Blades <tdblades@gmail.com>
2019-02-12 14:45:33 -0800

    Export components from base theme

--- packages/gatsby-theme-apollo/index.js
+++ packages/gatsby-theme-apollo/index.js
@@ -1,1 +1,8 @@
-// noop
+export {default as ContentWrapper} from './src/components/content-wrapper';
+export {default as FlexWrapper} from './src/components/flex-wrapper';
+export {default as Header} from './src/components/header';
+export {default as Layout} from './src/components/layout';
+export {default as Logo} from './src/components/logo';
+export {default as LogoTitle} from './src/components/logo-title';
+export {default as Sidebar} from './src/components/sidebar';
+export {default as SidebarNav} from './src/components/sidebar-nav';
