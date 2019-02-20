105d967dd12218ab3c42eebba3bef4bd57865292
Trevor Blades <tdblades@gmail.com>
2019-02-19 16:24:35 -0800

    Move docsNav to a render function in the docs templat

--- packages/gatsby-theme-apollo-docs/src/templates/docs/index.js
+++ packages/gatsby-theme-apollo-docs/src/templates/docs/index.js
@@ -1,8 +1,7 @@
-import DocsNav from './docs-nav';
 import Helmet from 'react-helmet';
 import PageContent from './page-content';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component} from 'react';
 import Search from './search';
 import VersionSelect from './version-select';
 import styled from '@emotion/styled';
@@ -44,74 +43,125 @@ const MainSubheading = styled.h3({
   color: colors.text2
 });
 
-export default function Docs(props) {
-  const {
-    version,
-    versions,
-    title,
-    description,
-    content,
-    fileDir
-  } = props.pageContext;
-  const {title: pageTitle, subtitle, basePath} = props.data.site.siteMetadata;
-  const docsNav = <DocsNav basePath={basePath} />;
-  return (
-    <Layout>
-      <Helmet>
-        <title>{title}</title>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
-        />
-      </Helmet>
-      <ResponsiveSidebar>
-        {({sidebarRef, onWrapperClick, openSidebar, sidebarOpen}) => (
-          <FlexWrapper onClick={onWrapperClick}>
-            <Sidebar
-              responsive
-              open={sidebarOpen}
-              ref={sidebarRef}
-              title={pageTitle}
-            >
-              <SidebarContentHeader>
-                {subtitle}
-                <VersionSelect versions={versions} value={version.basePath} />
-              </SidebarContentHeader>
-              <SidebarNav
-                contents={version.contents}
-                pathname={props.location.pathname}
-              />
-            </Sidebar>
-            <Main>
-              <MobileHeader>
-                <MenuButton onClick={openSidebar} />
-                <LogoTitle />
-                {docsNav}
-              </MobileHeader>
-              <DesktopHeader>
-                <Search />
-                {docsNav}
-              </DesktopHeader>
-              <ContentWrapper>
-                <div>
-                  <MainHeading>{title}</MainHeading>
-                  {description && (
-                    <MainSubheading>{description}</MainSubheading>
-                  )}
-                </div>
-                <hr />
-                <PageContent
-                  content={content}
-                  fileDir={fileDir}
-                  version={version}
+const Nav = styled.nav({
+  display: 'flex',
+  alignSelf: 'stretch',
+  margin: '0 40px'
+});
+
+const NavItem = styled.a({
+  display: 'flex',
+  alignItems: 'center',
+  padding: '0 4px',
+  borderBottom: '2px solid transparent',
+  fontSize: 18,
+  color: colors.primary,
+  textDecoration: 'none',
+  '&.active': {
+    borderColor: colors.secondary
+  },
+  ':not(:last-child)': {
+    marginRight: 24
+  }
+});
+
+const navItems = {
+  '/docs/platform': 'Platform',
+  '/docs/tutorial': 'Tutorial',
+  '/docs/client': 'Client',
+  '/docs/server': 'Server',
+  '/docs/community': 'Community'
+};
+
+export default class Docs extends Component {
+  renderHeaderNav() {
+    return (
+      <Nav>
+        {Object.keys(navItems).map(key => (
+          <NavItem
+            key={key}
+            href={key}
+            className={
+              key === this.props.data.site.siteMetadata.basePath
+                ? 'active'
+                : null
+            }
+          >
+            {navItems[key]}
+          </NavItem>
+        ))}
+      </Nav>
+    );
+  }
+
+  render() {
+    const {
+      version,
+      versions,
+      title,
+      description,
+      content,
+      fileDir
+    } = this.props.pageContext;
+    const {title: pageTitle, subtitle} = this.props.data.site.siteMetadata;
+    return (
+      <Layout>
+        <Helmet>
+          <title>{title}</title>
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
+          />
+        </Helmet>
+        <ResponsiveSidebar>
+          {({sidebarRef, onWrapperClick, openSidebar, sidebarOpen}) => (
+            <FlexWrapper onClick={onWrapperClick}>
+              <Sidebar
+                responsive
+                open={sidebarOpen}
+                ref={sidebarRef}
+                title={pageTitle}
+              >
+                <SidebarContentHeader>
+                  {subtitle}
+                  <VersionSelect versions={versions} value={version.basePath} />
+                </SidebarContentHeader>
+                <SidebarNav
+                  contents={version.contents}
+                  pathname={this.props.location.pathname}
                 />
-              </ContentWrapper>
-            </Main>
-          </FlexWrapper>
-        )}
-      </ResponsiveSidebar>
-    </Layout>
-  );
+              </Sidebar>
+              <Main>
+                <MobileHeader>
+                  <MenuButton onClick={openSidebar} />
+                  <LogoTitle />
+                  {this.renderHeaderNav()}
+                </MobileHeader>
+                <DesktopHeader>
+                  <Search />
+                  {this.renderHeaderNav()}
+                </DesktopHeader>
+                <ContentWrapper>
+                  <div>
+                    <MainHeading>{title}</MainHeading>
+                    {description && (
+                      <MainSubheading>{description}</MainSubheading>
+                    )}
+                  </div>
+                  <hr />
+                  <PageContent
+                    content={content}
+                    fileDir={fileDir}
+                    version={version}
+                  />
+                </ContentWrapper>
+              </Main>
+            </FlexWrapper>
+          )}
+        </ResponsiveSidebar>
+      </Layout>
+    );
+  }
 }
 
 Docs.propTypes = {
