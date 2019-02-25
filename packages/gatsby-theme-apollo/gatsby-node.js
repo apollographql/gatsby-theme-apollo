69c2565b7af7591dec3cdeb25d1a2eb9181aa7a4
Trevor Blades <tdblades@gmail.com>
2019-02-25 13:07:11 -0800

    Use hosted favicon

--- packages/gatsby-theme-apollo/gatsby-node.js
+++ packages/gatsby-theme-apollo/gatsby-node.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const path = require('path');
 
 // include theme files in babel transpilation
@@ -14,12 +13,4 @@ exports.onCreateWebpackConfig = ({loaders, actions}) => {
       ]
     }
   });
-};
-
-// copy the favicon from the theme dir to the built website
-exports.onPostBootstrap = (_, {root}) => {
-  fs.copyFileSync(
-    path.resolve(__dirname, 'static/favicon.ico'),
-    path.resolve(root, 'public/favicon.ico')
-  );
 };
