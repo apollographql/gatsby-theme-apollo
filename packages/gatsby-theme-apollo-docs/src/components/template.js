1ab51c07bb11b44e0c86cd2113888b72f096dc13
Trevor Blades <tdblades@gmail.com>
2019-03-01 15:17:07 -0800

    Use StaticQuery in the template component

--- packages/gatsby-theme-apollo-docs/src/components/template.js
+++ packages/gatsby-theme-apollo-docs/src/components/template.js
@@ -1,11 +1,10 @@
-import Helmet from 'react-helmet';
 import NavItem from './nav-item';
 import PageContent from './page-content';
 import PropTypes from 'prop-types';
 import React from 'react';
+import SEO from './seo';
 import Search from './search';
 import SelectLink from './select-link';
-import socialImage from '../assets/images/social.jpg';
 import styled from '@emotion/styled';
 import {
   ContentWrapper,
@@ -21,7 +20,7 @@ import {
   breakpoints,
   colors
 } from 'gatsby-theme-apollo';
-import {graphql} from 'gatsby';
+import {StaticQuery, graphql} from 'gatsby';
 
 const StyledLogoTitle = styled(LogoTitle)({
   marginRight: 'auto'
@@ -118,118 +117,101 @@ export default function Template(props) {
     filePath
   } = props.pageContext;
 
-  const {
-    title: siteTitle,
-    description: siteDescription,
-    subtitle
-  } = props.data.site.siteMetadata;
-
   return (
-    <Layout>
-      <Helmet>
-        <title>{title}</title>
-        <meta property="og:title" content={title} />
-        <meta property="og:site_name" content={siteTitle} />
-        <meta
-          property="og:description"
-          content={description || siteDescription}
-        />
-        <meta property="og:image" content={socialImage} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@apollographql" />
-        <meta name="twitter:title" content={title} />
-        <meta
-          name="twitter:description"
-          content={description || siteDescription}
-        />
-        <meta
-          name="twitter:image"
-          content={'https://apollographql.com' + socialImage}
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css"
-        />
-      </Helmet>
-      <ResponsiveSidebar>
-        {({sidebarRef, onWrapperClick, openSidebar, sidebarOpen}) => (
-          <FlexWrapper onClick={onWrapperClick}>
-            <Sidebar
-              responsive
-              open={sidebarOpen}
-              ref={sidebarRef}
-              title={siteTitle}
-            >
-              <SidebarContentHeader>
-                {subtitle}
-                <SelectLink
-                  value={version.basePath}
-                  options={versions.map(({majorMinor, basePath}) => ({
-                    text: `Version ${majorMinor}`,
-                    value: basePath
-                  }))}
-                />
-              </SidebarContentHeader>
-              <SidebarNav
-                contents={version.contents}
-                pathname={props.location.pathname}
-              />
-            </Sidebar>
-            <Main>
-              <MobileHeader>
-                <MenuButton onClick={openSidebar} />
-                <StyledLogoTitle />
-                <SelectLink
-                  large
-                  options={navOptions}
-                  value={version.basePath}
-                />
-              </MobileHeader>
-              <DesktopHeader>
-                <Search />
-                <Nav>
-                  {navOptions.map(({value, text, subpages}) => (
-                    <NavItem key={value} href={value} subpages={subpages}>
-                      {text}
-                    </NavItem>
-                  ))}
-                </Nav>
-              </DesktopHeader>
-              <ContentWrapper>
-                <div>
-                  <MainHeading>{title}</MainHeading>
-                  {description && (
-                    <MainSubheading>{description}</MainSubheading>
-                  )}
-                </div>
-                <hr />
-                <PageContent
-                  content={content}
-                  filePath={filePath}
-                  version={version}
-                />
-              </ContentWrapper>
-            </Main>
-          </FlexWrapper>
-        )}
-      </ResponsiveSidebar>
-    </Layout>
+    <StaticQuery
+      query={graphql`
+        {
+          site {
+            siteMetadata {
+              title
+              subtitle
+            }
+          }
+        }
+      `}
+      render={data => {
+        const {
+          title: siteName,
+          description: siteDescription,
+          subtitle
+        } = data.site.siteMetadata;
+        return (
+          <Layout>
+            <SEO
+              title={title}
+              description={description || siteDescription}
+              siteName={siteName}
+            />
+            <ResponsiveSidebar>
+              {({sidebarRef, onWrapperClick, openSidebar, sidebarOpen}) => (
+                <FlexWrapper onClick={onWrapperClick}>
+                  <Sidebar
+                    responsive
+                    open={sidebarOpen}
+                    ref={sidebarRef}
+                    title={siteName}
+                  >
+                    <SidebarContentHeader>
+                      {subtitle}
+                      <SelectLink
+                        value={version.basePath}
+                        options={versions.map(({majorMinor, basePath}) => ({
+                          text: `Version ${majorMinor}`,
+                          value: basePath
+                        }))}
+                      />
+                    </SidebarContentHeader>
+                    <SidebarNav
+                      contents={version.contents}
+                      pathname={props.location.pathname}
+                    />
+                  </Sidebar>
+                  <Main>
+                    <MobileHeader>
+                      <MenuButton onClick={openSidebar} />
+                      <StyledLogoTitle />
+                      <SelectLink
+                        large
+                        options={navOptions}
+                        value={version.basePath}
+                      />
+                    </MobileHeader>
+                    <DesktopHeader>
+                      <Search />
+                      <Nav>
+                        {navOptions.map(({value, text, subpages}) => (
+                          <NavItem key={value} href={value} subpages={subpages}>
+                            {text}
+                          </NavItem>
+                        ))}
+                      </Nav>
+                    </DesktopHeader>
+                    <ContentWrapper>
+                      <div>
+                        <MainHeading>{title}</MainHeading>
+                        {description && (
+                          <MainSubheading>{description}</MainSubheading>
+                        )}
+                      </div>
+                      <hr />
+                      <PageContent
+                        content={content}
+                        filePath={filePath}
+                        version={version}
+                      />
+                    </ContentWrapper>
+                  </Main>
+                </FlexWrapper>
+              )}
+            </ResponsiveSidebar>
+          </Layout>
+        );
+      }}
+    />
   );
 }
 
 Template.propTypes = {
-  data: PropTypes.object.isRequired,
   pageContext: PropTypes.object.isRequired,
   location: PropTypes.object.isRequired
 };
-
-export const pageQuery = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-        subtitle
-      }
-    }
-  }
-`;
