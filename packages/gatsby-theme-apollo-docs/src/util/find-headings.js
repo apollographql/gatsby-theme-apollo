7ace0e6ad6a8c57a1896cc8f29433e8010466f43
Trevor Blades <tdblades@gmail.com>
2019-03-07 16:03:47 -0800

    Don't render id-less headings in the contents menu

--- packages/gatsby-theme-apollo-docs/src/util/find-headings.js
+++ packages/gatsby-theme-apollo-docs/src/util/find-headings.js
@@ -2,12 +2,14 @@ export default function findHeadings(component) {
   return component.props.children.reduce((acc, child) => {
     if (typeof child === 'object') {
       if (/^h[1-6]$/.test(child.type)) {
-        return acc.concat([
-          {
-            id: child.props.id,
-            text: child.props.children[0]
-          }
-        ]);
+        return child.props.id
+          ? acc.concat([
+              {
+                id: child.props.id,
+                text: child.props.children[0]
+              }
+            ])
+          : acc;
       }
 
       if (Array.isArray(child.props.children)) {
