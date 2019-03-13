61dbbda1d26529446a3aed89085d21c2b0e50914
Trevor Blades <tdblades@gmail.com>
2019-03-13 16:16:56 -0700

    Don't list headings from tsapibox in the contents list

--- packages/gatsby-theme-apollo-docs/src/util/find-headings.js
+++ packages/gatsby-theme-apollo-docs/src/util/find-headings.js
@@ -1,6 +1,9 @@
 export default function findHeadings(component) {
   return component.props.children.reduce((acc, child) => {
-    if (typeof child === 'object') {
+    if (
+      typeof child === 'object' &&
+      (!child.props.className || child.props.className.indexOf('api'))
+    ) {
       if (/^h[1-6]$/.test(child.type)) {
         return child.props.id
           ? acc.concat([
