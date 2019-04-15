cb41fcc1341ae78aa445ea1bd7aff1c860d66590
Trevor Blades <tdblades@gmail.com>
2019-04-15 16:51:58 -0700

    WIP: use source/transform for pages, add mdx

---
+++ packages/gatsby-theme-apollo-docs/src/templates/mdx.js
@@ -0,0 +1,26 @@
+import MDXRenderer from 'gatsby-mdx/mdx-renderer';
+import React from 'react';
+import Template from '../components/template';
+import {graphql} from 'gatsby';
+
+export default function MDX({data}) {
+  return (
+    <Template location={props.location} {...props.data.mdx.frontmatter}>
+      <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
+    </Template>
+  );
+}
+
+export const pageQuery = graphql`
+  query MDX($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        description
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
