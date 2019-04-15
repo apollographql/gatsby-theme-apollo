5ff993dba6e765454e5168934191a70f12219664
Trevor Blades <tdblades@gmail.com>
2019-04-15 16:52:33 -0700

    Fix props

--- packages/gatsby-theme-apollo-docs/src/templates/mdx.js
+++ packages/gatsby-theme-apollo-docs/src/templates/mdx.js
@@ -3,10 +3,10 @@ import React from 'react';
 import Template from '../components/template';
 import {graphql} from 'gatsby';
 
-export default function MDX({data}) {
+export default function MDX(props) {
   return (
     <Template location={props.location} {...props.data.mdx.frontmatter}>
-      <MDXRenderer>{data.mdx.code.body}</MDXRenderer>
+      <MDXRenderer>{props.data.mdx.code.body}</MDXRenderer>
     </Template>
   );
 }
