023199a2191d57bf1cf2095d86ec72b558964b93
Trevor Blades <tdblades@gmail.com>
2019-04-24 16:51:35 -0700

    Remove unused dependencies

--- packages/gatsby-theme-apollo-docs/src/components/template.js
+++ packages/gatsby-theme-apollo-docs/src/components/template.js
@@ -11,7 +11,6 @@ import SEO from './seo';
 import Search from './search';
 import SelectLink from './select-link';
 import SidebarContent from './sidebar-content';
-import flatMap from 'lodash/flatMap';
 import styled from '@emotion/styled';
 import {
   ContentWrapper,
@@ -150,10 +149,6 @@ export default class Template extends PureComponent {
     } = this.props.pageContext;
 
     const [owner, repo] = githubRepo.split('/');
-    const pages = flatMap(sidebarContents, 'pages').filter(
-      page => !page.anchor
-    );
-
     return (
       <Layout>
         <SEO
@@ -198,7 +193,9 @@ export default class Template extends PureComponent {
                     repo={repo}
                     gitRef="master"
                     pathname={pathname}
-                    pages={pages}
+                    pages={sidebarContents
+                      .reduce((acc, {pages}) => acc.concat(pages), [])
+                      .filter(page => !page.anchor)}
                     headings={headings}
                     spectrumPath={spectrumPath}
                     filePath={filePath}
