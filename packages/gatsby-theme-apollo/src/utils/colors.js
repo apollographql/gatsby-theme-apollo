00dfe156638fbb39bae64c9a3d3c5b0d1f8025ed
Trevor Blades <tdblades@gmail.com>
2019-05-16 16:42:07 -0700

    Use space kit colors

--- packages/gatsby-theme-apollo/src/utils/colors.js
+++ packages/gatsby-theme-apollo/src/utils/colors.js
@@ -1,13 +1,14 @@
+import {colors} from '@apollo/space-kit';
+
 export default {
-  primary: '#220a82',
-  secondary: '#e535ab',
-  tertiary: '#009f97',
-  divider: '#d8d9e0',
-  background: '#f7f8fa',
-  background2: '#f3f4f6',
-  text1: '#343c5a',
-  text2: '#747790',
-  text3: '#9496aa',
-  text4: '#afb1c0',
+  primary: colors.indigo.dark,
+  secondary: colors.pink.base,
+  tertiary: colors.teal.base,
+  divider: colors.silver.dark,
+  background: colors.silver.light,
+  text1: colors.black.light,
+  text2: colors.grey.darker,
+  text3: colors.grey.base,
+  text4: colors.grey.lighter,
   hoverOpacity: 0.8
 };
