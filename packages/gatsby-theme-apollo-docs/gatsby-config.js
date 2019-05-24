3d1eb48fc03bd452fad32dbbcb4c26af7e5458ec
Trevor Blades <tdblades@gmail.com>
2019-05-24 15:44:39 -0700

    Allow theme consumer to pass exceptions to the link checker

--- packages/gatsby-theme-apollo-docs/gatsby-config.js
+++ packages/gatsby-theme-apollo-docs/gatsby-config.js
@@ -1,74 +1,82 @@
-const gatsbyRemarkPlugins = [
-  'gatsby-remark-autolink-headers',
-  {
-    resolve: 'gatsby-remark-copy-linked-files',
-    options: {
-      ignoreFileExtensions: []
-    }
-  },
-  {
-    resolve: 'gatsby-remark-prismjs',
-    options: {
-      showLineNumbers: true
-    }
-  },
-  'gatsby-remark-check-links'
-];
-
 module.exports = ({
   subtitle,
   description,
   root,
   githubRepo,
-  versions = {}
-}) => ({
-  __experimentalThemes: [
+  versions = {},
+  checkLinksExceptions
+}) => {
+  const gatsbyRemarkPlugins = [
+    'gatsby-remark-autolink-headers',
     {
-      resolve: 'gatsby-theme-apollo',
+      resolve: 'gatsby-remark-copy-linked-files',
       options: {
-        root
+        ignoreFileExtensions: []
+      }
+    },
+    {
+      resolve: 'gatsby-remark-prismjs',
+      options: {
+        showLineNumbers: true
+      }
+    },
+    {
+      resolve: 'gatsby-remark-check-links',
+      options: {
+        exceptions: checkLinksExceptions
       }
     }
-  ],
-  siteMetadata: {
-    title: 'Apollo Docs',
-    subtitle,
-    description
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${root}/source`,
-        name: 'docs'
+  ];
+
+  return {
+    __experimentalThemes: [
+      {
+        resolve: 'gatsby-theme-apollo',
+        options: {
+          root
+        }
       }
+    ],
+    siteMetadata: {
+      title: 'Apollo Docs',
+      subtitle,
+      description
     },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: gatsbyRemarkPlugins
-      }
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-74643563-13'
-      }
-    },
-    {
-      resolve: 'gatsby-mdx',
-      options: {
-        gatsbyRemarkPlugins
-      }
-    },
-    ...Object.keys(versions).map(key => ({
-      resolve: 'gatsby-source-git',
-      options: {
-        name: key,
-        remote: `https://github.com/${githubRepo}`,
-        branch: versions[key],
-        patterns: 'docs/**'
-      }
-    }))
-  ]
-});
+    plugins: [
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          path: `${root}/source`,
+          name: 'docs'
+        }
+      },
+      {
+        resolve: 'gatsby-transformer-remark',
+        options: {
+          plugins: gatsbyRemarkPlugins
+        }
+      },
+      {
+        resolve: 'gatsby-plugin-google-analytics',
+        options: {
+          trackingId: 'UA-74643563-13'
+        }
+      },
+      {
+        resolve: 'gatsby-mdx',
+        options: {
+          gatsbyRemarkPlugins
+        }
+      },
+      ...Object.keys(versions).map(key => ({
+        resolve: 'gatsby-source-git',
+        options: {
+          name: key,
+          remote: `https://github.com/${githubRepo}`,
+          branch: versions[key],
+          patterns: 'docs/**'
+        }
+      }))
+    ]
+  };
+};
