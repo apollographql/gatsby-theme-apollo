83a4631ff4557e69f4281aa346ae82a31798a85b
Trevor Blades <tdblades@gmail.com>
2019-08-29 13:19:38 -0700

    Add nav item descriptions

--- packages/gatsby-theme-apollo-docs/theme-options.js
+++ packages/gatsby-theme-apollo-docs/theme-options.js
@@ -1,15 +1,25 @@
 const navConfig = {
   '/docs': {
-    text: 'Platform',
+    text: 'Home',
+    description:
+      'Learn about the Apollo Platform; an implementation of GraphQL to manage data from the cloud to your UI.',
     matchRegex:
       '^/docs/(?!(tutorial|react|angular|link|ios|android|scalajs|apollo-server|graphql-tools|graphql-subscriptions|community))'
   },
-  '/docs/tutorial/introduction': {
-    text: 'Tutorial',
-    matchRegex: '^/docs/tutorial'
+  '/docs/apollo-server': {
+    text: 'Server',
+    description:
+      'Quickly build a production-ready, self-documenting API for GraphQL clients, using data from any source.',
+    subpages: {
+      '/docs/apollo-server': 'Apollo Server',
+      '/docs/graphql-tools': 'graphql-tools',
+      '/docs/graphql-subscriptions': 'GraphQL subscriptions'
+    }
   },
   '/docs/react': {
-    text: 'Client',
+    text: 'Client (React)',
+    description:
+      'A complete Javascript library that takes care of requesting and caching your data, as well as updating your UI.',
     subpages: {
       '/docs/react': 'React + React Native',
       '/docs/angular': 'Angular',
@@ -20,26 +30,37 @@ const navConfig = {
       '/docs/scalajs': 'Scala.js'
     }
   },
-  '/docs/apollo-server': {
-    text: 'Server',
-    subpages: {
-      '/docs/apollo-server': 'Apollo Server',
-      '/docs/graphql-tools': 'graphql-tools',
-      '/docs/graphql-subscriptions': 'GraphQL subscriptions'
-    }
+  '/docs/platform/graph-manager-overview': {
+    text: 'Graph Manager',
+    description:
+      'A guide to our cloud service for schema management and performance metrics monitoring.'
   },
-  '/docs/community': {
-    text: 'Community',
-    subpages: {
-      'https://blog.apollographql.com': 'Blog',
-      'https://spectrum.chat/apollo': 'Spectrum',
-      'https://twitter.com/apollographql': 'Twitter',
-      'https://youtube.com/channel/UC0pEW_GOrMJ23l8QcrGdKSw': 'YouTube',
-      '/docs/community': 'Contribute',
-      'https://summit.graphql.com': 'GraphQL Summit',
-      'https://graphql.com': 'Explore GraphQL'
-    }
+  '/docs/ios': {
+    text: 'Client (iOS)',
+    description:
+      'A strongly-typed, caching GraphQL client for native iOS apps, written in Swift.'
+  },
+  '/docs/link': {
+    text: 'Link',
+    description:
+      'Designed to be a powerful way to compose actions around data handling with GraphQL.'
   }
+  // '/docs/community': {
+  //   text: 'Community',
+  //   subpages: {
+  //     'https://blog.apollographql.com': 'Blog',
+  //     'https://spectrum.chat/apollo': 'Spectrum',
+  //     'https://twitter.com/apollographql': 'Twitter',
+  //     'https://youtube.com/channel/UC0pEW_GOrMJ23l8QcrGdKSw': 'YouTube',
+  //     '/docs/community': 'Contribute',
+  //     'https://summit.graphql.com': 'GraphQL Summit',
+  //     'https://graphql.com': 'Explore GraphQL'
+  //   }
+  // },
+  // '/docs/tutorial/introduction': {
+  //   text: 'Tutorial',
+  //   matchRegex: '^/docs/tutorial'
+  // }
 };
 
 module.exports = {
