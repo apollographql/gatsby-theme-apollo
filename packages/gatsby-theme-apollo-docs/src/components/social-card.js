dda2f68568d3e87267ee5b0f66382c67e5d79124
Trevor Blades <tdblades@gmail.com>
2019-09-26 16:18:29 -0700

    Add preval back

--- packages/gatsby-theme-apollo-docs/src/components/social-card.js
+++ packages/gatsby-theme-apollo-docs/src/components/social-card.js
@@ -1,8 +1,37 @@
+/* global preval */
 import PropTypes from 'prop-types';
 import React from 'react';
 import TextFit from 'react-textfit';
 import {ApolloIcon} from '@apollo/space-kit/icons/ApolloIcon';
+import {Global, css} from '@emotion/core';
 import {colors} from 'gatsby-theme-apollo-core/src/utils/colors';
+
+const stuff = preval`
+  const fs = require('fs')
+  const path = require('path')
+  const fontDir = path.dirname(
+    require.resolve('source-sans-pro/source-sans-pro.css')
+  )
+
+  const base64 = fs.readFileSync(
+    fontDir + '/WOFF2/TTF/SourceSansPro-Regular.ttf.woff2',
+    'base64'
+  )
+
+  const fonts = fs
+    .readFileSync(
+      path.join(fontDir, '/source-sans-pro.css'),
+      "utf-8"
+    )
+    .replace(
+      'WOFF2/TTF/SourceSansPro-Regular.ttf.woff2',
+      'data:application/x-font-woff;charset=utf-8;base64,' + base64
+    )
+
+  module.exports = {
+    fonts
+  }
+`;
 
 const borderWidth = 6;
 const borderOffset = borderWidth * 2;
@@ -18,9 +47,15 @@ export default function SocialCard(props) {
         width: 1200 - borderOffset,
         height: 675 - borderOffset,
         padding: 80,
-        border: `${borderWidth}px solid ${colors.primary}`
+        border: `${borderWidth}px solid ${colors.primary}`,
+        fontFamily: "'Source Sans Pro'"
       }}
     >
+      <Global
+        styles={css`
+          ${stuff.fonts}
+        `}
+      />
       <div
         style={{
           fontSize: 50,
