f00a222c8403c75df489954b4591d02e04bd9781
Trevor Blades <tdblades@gmail.com>
2019-09-26 14:50:16 -0700

    Add some more card styles and title

--- packages/gatsby-theme-apollo-docs/src/components/social-card.js
+++ packages/gatsby-theme-apollo-docs/src/components/social-card.js
@@ -1,6 +1,40 @@
+/* global preval */
 import PropTypes from 'prop-types';
 import React from 'react';
 import TextFit from 'react-textfit';
+import {ApolloIcon} from '@apollo/space-kit/icons/ApolloIcon';
+import {Global, css} from '@emotion/core';
+import {colors} from 'gatsby-theme-apollo-core/src/utils/colors';
+
+const stuff = preval`
+  const fs = require('fs')
+  const path = require('path')
+  const fontDir = path.dirname(
+    require.resolve('source-sans-pro/source-sans-pro.css')
+  )
+  
+  const base64 = fs.readFileSync(
+    fontDir + '/WOFF2/TTF/SourceSansPro-Regular.ttf.woff2',
+    'base64'
+  )
+
+  const fonts = fs
+    .readFileSync(
+      path.join(fontDir, '/source-sans-pro.css'),
+      "utf-8"
+    )
+    .replace(
+      'WOFF2/TTF/SourceSansPro-Regular.ttf.woff2',
+      'data:application/x-font-woff;charset=utf-8;base64,' + base64
+    )
+      
+  module.exports = {
+    fonts
+  }
+`;
+
+const borderWidth = 6;
+const borderOffset = borderWidth * 2;
 
 export default function SocialCard(props) {
   return (
@@ -8,18 +42,50 @@ export default function SocialCard(props) {
       style={{
         display: 'flex',
         flexDirection: 'column',
-        width: 1200,
-        height: 675,
-        padding: 100
+        alignItems: 'flex-start',
+        boxSizing: 'border-box',
+        width: 1200 - borderOffset,
+        height: 675 - borderOffset,
+        padding: 80,
+        border: `${borderWidth}px solid ${colors.primary}`,
+        fontFamily: "'Source Sans Pro'"
       }}
     >
-      <h5 style={{marginBottom: 'auto'}}>Apollo Docs</h5>
-      <TextFit style={{height: 250}}>{props.frontmatter.title}</TextFit>
-      {/* <h3>{props.frontmatter.description}</h3> */}
+      <Global
+        styles={css`
+          ${stuff.fonts}
+        `}
+      />
+      <div
+        style={{
+          fontSize: 50,
+          marginBottom: 20
+        }}
+      >
+        {props.subtitle}
+      </div>
+      <TextFit
+        min={80}
+        max={140}
+        style={{
+          width: '100%',
+          height: 250,
+          marginBottom: 'auto'
+        }}
+      >
+        {props.title}
+      </TextFit>
+      <ApolloIcon
+        height={80}
+        style={{
+          color: colors.primary
+        }}
+      />
     </div>
   );
 }
 
 SocialCard.propTypes = {
-  frontmatter: PropTypes.object.isRequired
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired
 };
