4e1524952bedb3527cf15210994c78f758ffcd25
Trevor Blades <tdblades@gmail.com>
2019-09-26 12:59:57 -0700

    Use textfit

--- packages/gatsby-theme-apollo-docs/src/components/social-card.js
+++ packages/gatsby-theme-apollo-docs/src/components/social-card.js
@@ -1,10 +1,21 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import TextFit from 'react-textfit';
 
 export default function SocialCard(props) {
   return (
-    <div>
-      <h1>{props.frontmatter.title}</h1>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: 1200,
+        height: 675,
+        padding: 100
+      }}
+    >
+      <h5 style={{marginBottom: 'auto'}}>Apollo Docs</h5>
+      <TextFit style={{height: 250}}>{props.frontmatter.title}</TextFit>
+      {/* <h3>{props.frontmatter.description}</h3> */}
     </div>
   );
 }
