37bc4d68ea2116b1f15d7a634d5d7fe06245b1cf
Trevor Blades <tdblades@gmail.com>
2019-09-26 17:10:17 -0700

    Prefix social image

--- packages/gatsby-theme-apollo-docs/src/components/seo.js
+++ packages/gatsby-theme-apollo-docs/src/components/seo.js
@@ -1,23 +1,25 @@
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 import React from 'react';
+import {withPrefix} from 'gatsby';
 
 export default function SEO(props) {
   const {title, description, siteName, twitterHandle, baseUrl} = props;
+  const imagePath = withPrefix('/' + props.image);
   return (
     <Helmet>
       <title>{title}</title>
       <meta property="og:title" content={title} />
       <meta property="og:site_name" content={siteName} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={'/' + props.image} />
+      <meta property="og:image" content={imagePath} />
       <meta name="twitter:card" content="summary_large_image" />
       {twitterHandle && (
         <meta name="twitter:site" content={`@${twitterHandle}`} />
       )}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={`${baseUrl}/${props.image}`} />
+      <meta name="twitter:image" content={baseUrl + imagePath} />
     </Helmet>
   );
 }
