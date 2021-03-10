0eeda094596833154744592cf194d02d47f1c7e5
Jay Chesterman <jay.chesterman@live.ca>
2021-03-10 16:04:03 -0500

    Update comment

--- packages/gatsby-theme-apollo-docs/theme-options.js
+++ packages/gatsby-theme-apollo-docs/theme-options.js
@@ -81,7 +81,7 @@ module.exports = {
   menuTitle: 'Apollo Platform',
   gaTrackingId: [
     'UA-74643563-13',
-    'G-0BGG5V2W2K' // unified ga4 property
+    'G-0BGG5V2W2K' // unified ga property
   ],
   algoliaApiKey: '768e823959d35bbd51e4b2439be13fb7',
   algoliaIndexName: 'apollodata',
