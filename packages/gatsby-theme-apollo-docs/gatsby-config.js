31c28abec44da5355b22014477150d5ba7914a04
Trevor Blades <tdblades@gmail.com>
2021-07-15 15:38:43 -0700

    Move transform function into theme and add an isCurrentVersion field

--- packages/gatsby-theme-apollo-docs/gatsby-config.js
+++ packages/gatsby-theme-apollo-docs/gatsby-config.js
@@ -2,7 +2,83 @@ const path = require('path');
 const remarkTypescript = require('remark-typescript');
 const {colors} = require('gatsby-theme-apollo-core/src/utils/colors');
 const {HEADER_HEIGHT} = require('./src/utils');
-const {queries, parse, algoliaSettings} = require('apollo-algolia-transform');
+const cheerio = require('cheerio');
+const {
+  getMdxHeadings,
+  algoliaSettings,
+  MetricsFetcher,
+  METRICS
+} = require('apollo-algolia-transform');
+
+function getMdHeadings(tableOfContents) {
+  const $ = cheerio.load(tableOfContents);
+
+  return $('a')
+    .get()
+    .reduce((acc, aTag) => {
+      const el = $(aTag);
+      const depth = el.parents('ul').length + 1;
+      const href = el.attr('href');
+      const hashIndex = href.indexOf('#');
+
+      const heading = {
+        url: href.slice(hashIndex),
+        title: el.text()
+      };
+
+      const key = 'h' + depth;
+      const existing = acc[key];
+
+      return {
+        ...acc,
+        [key]: existing ? [...existing, heading] : [heading]
+      };
+    }, {});
+}
+
+async function parse({data, baseUrl, viewId}) {
+  let allGAData = {};
+  if (process.env.NODE_ENV !== 'test') {
+    const metricsFetcher = new MetricsFetcher({viewId});
+    allGAData = await metricsFetcher.fetchAll();
+  }
+
+  const {site, allMarkdownRemark, allMdx} = data;
+  const allPages = allMarkdownRemark.nodes.concat(allMdx.nodes);
+  return allPages.flatMap(page => {
+    const {id, fields, frontmatter, excerpt, tableOfContents, mdxAST} = page;
+    const {slug, sidebarTitle, isCurrentVersion} = fields;
+    const {title} = frontmatter;
+
+    const url = baseUrl + site.pathPrefix + slug;
+    const docset = site.pathPrefix.replace(/^\/docs\//, '');
+
+    const categories = ['documentation', sidebarTitle.toLowerCase()];
+    if (['react', 'ios', 'android'].includes(docset)) {
+      categories.push('client');
+    }
+
+    // TODO: split page into sections by heading
+    return [
+      {
+        objectID: id,
+        index: 0, // TODO: make dynamic
+        type: 'docs',
+        url,
+        docset,
+        title,
+        excerpt,
+        categories,
+        isCurrentVersion,
+        headings: mdxAST
+          ? getMdxHeadings(tableOfContents.items)
+          : getMdHeadings(tableOfContents),
+        pageviews: allGAData[url]?.[METRICS.uniquePageViews] || 0
+        // TODO: needs text, probably other properties
+      }
+    ];
+  });
+}
 
 module.exports = ({
   root,
@@ -191,11 +267,53 @@ module.exports = ({
         skipIndexing: process.env.CONTEXT !== 'production',
         queries: [
           {
-            query: queries.docs,
+            query: `
+              {
+                site {
+                  pathPrefix
+                }
+                allMarkdownRemark {
+                  nodes {
+                    id
+                    htmlAst
+                    excerpt(pruneLength: 100)
+                    tableOfContents
+                    frontmatter {
+                      title
+                      description
+                    }
+                    fields {
+                      slug
+                      isCurrentVersion
+                      apiReference
+                      sidebarTitle
+                    }
+                  }
+                }
+                allMdx {
+                  nodes {
+                    id
+                    mdxAST
+                    excerpt(pruneLength: 100)
+                    tableOfContents
+                    frontmatter {
+                      title
+                      description
+                    }
+                    fields {
+                      slug
+                      isCurrentVersion
+                      apiReference
+                      sidebarTitle
+                    }
+                  }
+                }
+              }
+            `,
             transformer: ({data}) =>
               parse({
                 data,
-                baseUrl: baseUrl + pathPrefix,
+                baseUrl,
                 viewId: gaViewId
               }),
             indexName: algoliaIndexName,
