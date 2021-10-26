104757e4dfc2d513459ee2eacd0a177d166ba726
Stephen Barlow <stephen@apollographql.com>
2021-10-25 20:22:40 -0700

    Adjust min/max for code columns

--- packages/gatsby-theme-apollo-docs/src/components/code-columns.js
+++ packages/gatsby-theme-apollo-docs/src/components/code-columns.js
@@ -4,13 +4,13 @@ import styled from '@emotion/styled';
 export function CodeColumns(props) {
   const ThreeColumns = styled.div({
     display: 'grid',
-    gridTemplateColumns: 'repeat(3, minmax(0, 1fr))',
-    gridGap: '10px'
+    gridTemplateColumns: 'repeat(3, minmax(0, 0.33fr))',
+    gridGap: '5px'
   });
 
   const TwoColumns = styled.div({
     display: 'grid',
-    gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+    gridTemplateColumns: 'repeat(2, minmax(0, 0.5fr))',
     gridGap: '20px'
   });
 
